1db71149b2bee2a545cd83fac42aed49cf77bc8a
Sergio Aparicio <Curso@Formacion-TOSHh.ipartekaula.com>
2017-02-06 13:36:18 +0100

    Actualizamos la base de datos mysql

--- GestionDocenteSpring/src/main/resources/gestiondocente.sql
+++ GestionDocenteSpring/src/main/resources/gestiondocente.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-02-2017 a las 11:00:04
+-- Tiempo de generación: 06-02-2017 a las 13:33:30
 -- Versión del servidor: 5.6.17
 -- Versión de PHP: 5.5.12
 
@@ -19,9 +19,69 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestiondocente`
 --
-DROP DATABASE `gestiondocente`;
 CREATE DATABASE IF NOT EXISTS `gestiondocente` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `gestiondocente`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `fNacimiento` date DEFAULT NULL,
+  `direccion` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poblacion` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `codigoPostal` int(5) DEFAULT NULL,
+  `telefono` int(9) NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  `dni` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `nHermanos` int(2) DEFAULT NULL,
+  `activo` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `alumno`
+--
+
+INSERT INTO `alumno` (`codigo`, `nombre`, `apellidos`, `fNacimiento`, `direccion`, `poblacion`, `codigoPostal`, `telefono`, `email`, `dni`, `nHermanos`, `activo`) VALUES
+(1, 'Sergio', 'Aparicio Vegas', '1977-12-01', 'tartanga 8F Atico A', 'Erandiuo', 48950, 944062586, 'coritelsergio@hotmail.com', '44974398Z', 3, 1),
+(4, 'Paco', 'Perez Garcia', NULL, NULL, NULL, NULL, 607519862, 'pacogarcia@hotmail.com', '30694324L', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+CREATE TABLE IF NOT EXISTS `profesor` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `nSS` bigint(12) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+  `fNacimiento` date DEFAULT NULL,
+  `dni` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `direccion` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `poblacion` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `codigoPostal` int(5) DEFAULT NULL,
+  `telefono` int(9) NOT NULL,
+  `email` varchar(150) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `profesor`
+--
+
+INSERT INTO `profesor` (`codigo`, `nSS`, `nombre`, `apellidos`, `fNacimiento`, `dni`, `direccion`, `poblacion`, `codigoPostal`, `telefono`, `email`) VALUES
+(1, 123456789321, 'Consuelo', 'Mejia Aranzabal', '1919-02-04', '30694324L', 'Tartanga 8F', 'Erandio', 48950, 664384071, 'sapariciov@hotmail.com'),
+(2, 987654321123, 'Malika', 'Snow Zalabarria', '1994-05-05', '45678932V', 'Hidden Hills SN', 'Calabasas', 20001, 607118343, 'malika@correo.com');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
